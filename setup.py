5caee9218efe4be40f23410864faff80a10538dc
Teemu Rytilahti <tpr@iki.fi>
2017-01-14 03:48:23 +0100

    bump bluepy requirement to 1.0.5

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 
 setup(
     name='python-eq3bt',
-    version='0.1',
+    version='0.1.1',
     packages=['eq3bt'],
-    install_requires=['bluepy>=1.0.4'],
+    install_requires=['bluepy>=1.0.5'],
     description='EQ3 bluetooth thermostat support library',
     author='Markus Peter',
     author_email='mpeter@emdev.de',
