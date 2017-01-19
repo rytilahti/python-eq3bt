9cfe5f1e01ea225ee94ac58db66eab99cf3c8fa4
Teemu Rytilahti <tpr@iki.fi>
2017-01-19 02:59:56 +0100

    Fix setup.py typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='python-eq3bt',
     version='0.1.4',
     packages=['eq3bt'],
-    install_requires=['bluepy>=1.0.5', 'click', 'construct', 'click-datetime],
+    install_requires=['bluepy>=1.0.5', 'click', 'construct', 'click-datetime'],
     description='EQ3 bluetooth thermostat support library',
     author='Markus Peter',
     author_email='mpeter@emdev.de',
