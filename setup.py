c16244eb39f45e72d4540b34b19f491c118d2f44
Teemu Rytilahti <tpr@iki.fi>
2018-02-18 22:32:24 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='python-eq3bt',
-    version='0.1.8',
+    version='0.1.9',
     packages=['eq3bt'],
     install_requires=['bluepy>=1.0.5', 'click', 'construct', 'click-datetime'],
     description='EQ3 bluetooth thermostat support library',
