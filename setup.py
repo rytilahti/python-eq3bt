31fd093c8690f5c008e29e748075fdf2c0e1e947
Teemu Rytilahti <tpr@iki.fi>
2018-03-04 16:35:05 +0100

    require python 3.4 or newer in setup.py, closes #23

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name='python-eq3bt',
     version='0.1.9',
     packages=['eq3bt'],
+    python_requires='>=3.4',
     install_requires=['bluepy>=1.0.5', 'click', 'construct', 'click-datetime'],
     description='EQ3 bluetooth thermostat support library',
     author='Markus Peter',
