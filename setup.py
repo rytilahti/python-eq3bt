595459d9885920cf13b7059a1edd2cf38cede1f0
Teemu Rytilahti <tpr@iki.fi>
2018-11-09 20:28:44 +0100

    Bump version to 0.1.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='python-eq3bt',
-    version='0.1.9',
+    version='0.1.10',
     packages=['eq3bt'],
     python_requires='>=3.4',
     install_requires=['bluepy>=1.0.5', 'click', 'construct', 'click-datetime'],
