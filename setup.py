9946ff402ec83b819d43b496b863605ed14fbecc
Hummel95 <Hummel95@users.noreply.github.com>
2021-11-19 16:54:54 +0100

    Prepare 0.1.12 (#45)
    * Add bt interface selection
    
    * Update eq3cli.py
    
    Fix interface selection in cli
    
    * Update setup.py
    
    Bump version to 0.1.12
    
    * Update CHANGELOG

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='python-eq3bt',
-    version='0.1.11',
+    version='0.1.12',
     packages=['eq3bt'],
     python_requires='>=3.4',
     install_requires=['bluepy>=1.0.5', 'click', 'construct', 'click-datetime'],
