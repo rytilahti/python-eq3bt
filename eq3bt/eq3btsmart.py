rytilahti/python-eq3bt

commit 4e19f27c1c74fc131b6d98595314e540d3aac3fc

Use poetry, add pre-commit hooks & mass format to modern standards, add CI (#47)
* Use poetry, add pre-commit hooks & mass format to modern standards, add CI

* Remove requirements.txt, add codecov dep

* Add construct dep

* Remove mac & windows builds as bluepy is linux-only

eq3bt/eq3btsmart.py
@@ -7,12 +7,13 @@ To get the current state, update() has to be called for powersaving reasons.
 Schedule needs to be requested with query_schedule() before accessing for similar reasons.
 """
 
+import codecs
 import logging
 import struct
-import codecs
 from datetime import datetime, timedelta
-from construct import Byte
 from enum import IntEnum
+
+from construct import Byte
 
 from .connection import BTLEConnection
 from .structures import AwayDataAdapter, DeviceId, Schedule, Status
@@ -47,7 +48,8 @@ EQ3BT_ON_TEMP = 30.0
 
 
 class Mode(IntEnum):
-    """ Thermostat modes. """
+    """Thermostat modes."""
+
     Unknown = -1
     Closed = 0
     Open = 1
@@ -57,13 +59,12 @@ class Mode(IntEnum):
     Boost = 5
 
 
-MODE_NOT_TEMP = [Mode.Unknown,
-                 Mode.Closed,
-                 Mode.Open]
+MODE_NOT_TEMP = [Mode.Unknown, Mode.Closed, Mode.Open]
 
 
 class TemperatureException(Exception):
     """Temperature out of range error."""
+
     pass
 
 
@@ -102,18 +103,20 @@ class Thermostat:
         if self.away_end:
             away_end = "end: %s" % self._away_end
 
-        return "[%s] Target %s (mode: %s, away: %s)" % (self._conn.mac,
-                                                        self.target_temperature,
-                                                        self.mode_readable,
-                                                        away_end)
+        return "[{}] Target {} (mode: {}, away: {})".format(
+            self._conn.mac, self.target_temperature, self.mode_readable, away_end
+        )
 
     def _verify_temperature(self, temp):
         """Verifies that the temperature is valid.
-            :raises TemperatureException: On invalid temperature.
+        :raises TemperatureException: On invalid temperature.
         """
         if temp < self.min_temp or temp > self.max_temp:
-            raise TemperatureException('Temperature {} out of range [{}, {}]'
-                                       .format(temp, self.min_temp, self.max_temp))
+            raise TemperatureException(
+                "Temperature {} out of range [{}, {}]".format(
+                    temp, self.min_temp, self.max_temp
+                )
+            )
 
     def parse_schedule(self, data):
         """Parses the device sent schedule."""
@@ -127,7 +130,7 @@ class Thermostat:
         _LOGGER.debug("Received notification from the device..")
 
         if data[0] == PROP_INFO_RETURN and data[1] == 1:
-            _LOGGER.debug("Got status: %s" % codecs.encode(data, 'hex'))
+            _LOGGER.debug("Got status: %s" % codecs.encode(data, "hex"))
             status = Status.parse(data)
             _LOGGER.debug("Parsed status: %s", status)
 
@@ -168,8 +171,7 @@ class Thermostat:
             _LOGGER.debug("Mode:             %s", self.mode_readable)
             _LOGGER.debug("Target temp:      %s", self._target_temperature)
             _LOGGER.debug("Away end:         %s", self._away_end)
-            _LOGGER.debug("Window open temp: %s",
-                          self._window_open_temperature)
+            _LOGGER.debug("Window open temp: %s", self._window_open_temperature)
             _LOGGER.debug("Window open time: %s", self._window_open_time)
             _LOGGER.debug("Comfort temp:     %s", self._comfort_temperature)
             _LOGGER.debug("Eco temp:         %s", self._eco_temperature)
@@ -186,21 +188,30 @@ class Thermostat:
             self._device_serial = parsed.serial
 
         else:
-            _LOGGER.debug("Unknown notification %s (%s)", data[0], codecs.encode(data, 'hex'))
+            _LOGGER.debug(
+                "Unknown notification %s (%s)", data[0], codecs.encode(data, "hex")
+            )
 
     def query_id(self):
         """Query device identification information, e.g. the serial number."""
         _LOGGER.debug("Querying id..")
-        value = struct.pack('B', PROP_ID_QUERY)
+        value = struct.pack("B", PROP_ID_QUERY)
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
     def update(self):
         """Update the data from the thermostat. Always sets the current time."""
         _LOGGER.debug("Querying the device..")
         time = datetime.now()
-        value = struct.pack('BBBBBBB', PROP_INFO_QUERY,
-                            time.year % 100, time.month, time.day,
-                            time.hour, time.minute, time.second)
+        value = struct.pack(
+            "BBBBBBB",
+            PROP_INFO_QUERY,
+            time.year % 100,
+            time.month,
+            time.day,
+            time.hour,
+            time.minute,
+            time.second,
+        )
 
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
@@ -210,19 +221,19 @@ class Thermostat:
         if day < 0 or day > 6:
             _LOGGER.error("Invalid day: %s", day)
 
-        value = struct.pack('BB', PROP_SCHEDULE_QUERY, day)
+        value = struct.pack("BB", PROP_SCHEDULE_QUERY, day)
 
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
     @property
     def schedule(self):
-        """ Returns previously fetched schedule.
-         :return: Schedule structure or None if not fetched.
-         """
+        """Returns previously fetched schedule.
+        :return: Schedule structure or None if not fetched.
+        """
         return self._schedule
 
     def set_schedule(self, data):
-        """Sets the schedule for the given day. """
+        """Sets the schedule for the given day."""
         value = Schedule.build(data)
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
@@ -237,10 +248,10 @@ class Thermostat:
         dev_temp = int(temperature * 2)
         if temperature == EQ3BT_OFF_TEMP or temperature == EQ3BT_ON_TEMP:
             dev_temp |= 0x40
-            value = struct.pack('BB', PROP_MODE_WRITE, dev_temp)
+            value = struct.pack("BB", PROP_MODE_WRITE, dev_temp)
         else:
             self._verify_temperature(temperature)
-            value = struct.pack('BB', PROP_TEMPERATURE_WRITE, dev_temp)
+            value = struct.pack("BB", PROP_TEMPERATURE_WRITE, dev_temp)
 
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
@@ -269,8 +280,9 @@ class Thermostat:
             return self.set_mode(0x40 | int(EQ3BT_ON_TEMP * 2))
 
         if mode == Mode.Manual:
-            temperature = max(min(self._target_temperature, self.max_temp),
-                              self.min_temp)
+            temperature = max(
+                min(self._target_temperature, self.max_temp), self.min_temp
+            )
             return self.set_mode(0x40 | int(temperature * 2))
         else:
             return self.set_mode(0)
@@ -280,8 +292,8 @@ class Thermostat:
         return self._away_end
 
     def set_away(self, away_end=None, temperature=EQ3BT_AWAY_TEMP):
-        """ Sets away mode with target temperature.
-            When called without parameters disables away mode."""
+        """Sets away mode with target temperature.
+        When called without parameters disables away mode."""
         if not away_end:
             _LOGGER.debug("Disabling away, going to auto mode.")
             return self.set_mode(0x00)
@@ -293,7 +305,7 @@ class Thermostat:
         self.set_mode(0x80 | int(temperature * 2), packed)
 
     def set_mode(self, mode, payload=None):
-        value = struct.pack('BB', PROP_MODE_WRITE, mode)
+        value = struct.pack("BB", PROP_MODE_WRITE, mode)
         if payload:
             value += payload
         self._conn.make_request(PROP_WRITE_HANDLE, value)
@@ -339,7 +351,7 @@ class Thermostat:
     def boost(self, boost):
         """Sets boost mode."""
         _LOGGER.debug("Setting boost mode: %s", boost)
-        value = struct.pack('BB', PROP_BOOST, bool(boost))
+        value = struct.pack("BB", PROP_BOOST, bool(boost))
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
     @property
@@ -350,19 +362,25 @@ class Thermostat:
     @property
     def window_open(self):
         """Returns True if the thermostat reports a open window
-           (detected by sudden drop of temperature)"""
+        (detected by sudden drop of temperature)"""
         return self._raw_mode and self._raw_mode.WINDOW
 
     def window_open_config(self, temperature, duration):
         """Configures the window open behavior. The duration is specified in
         5 minute increments."""
-        _LOGGER.debug("Window open config, temperature: %s duration: %s", temperature, duration)
+        _LOGGER.debug(
+            "Window open config, temperature: %s duration: %s", temperature, duration
+        )
         self._verify_temperature(temperature)
         if duration.seconds < 0 and duration.seconds > 3600:
             raise ValueError
 
-        value = struct.pack('BBB', PROP_WINDOW_OPEN_CONFIG,
-                            int(temperature * 2), int(duration.seconds / 300))
+        value = struct.pack(
+            "BBB",
+            PROP_WINDOW_OPEN_CONFIG,
+            int(temperature * 2),
+            int(duration.seconds / 300),
+        )
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
     @property
@@ -384,7 +402,7 @@ class Thermostat:
     def locked(self, lock):
         """Locks or unlocks the thermostat."""
         _LOGGER.debug("Setting the lock: %s", lock)
-        value = struct.pack('BB', PROP_LOCK, bool(lock))
+        value = struct.pack("BB", PROP_LOCK, bool(lock))
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
     @property
@@ -398,8 +416,9 @@ class Thermostat:
         _LOGGER.debug("Setting temperature presets, comfort: %s eco: %s", comfort, eco)
         self._verify_temperature(comfort)
         self._verify_temperature(eco)
-        value = struct.pack('BBB', PROP_COMFORT_ECO_CONFIG, int(comfort * 2),
-                            int(eco * 2))
+        value = struct.pack(
+            "BBB", PROP_COMFORT_ECO_CONFIG, int(comfort * 2), int(eco * 2)
+        )
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
     @property
@@ -432,17 +451,17 @@ class Thermostat:
             values[current] = i
             current += 0.5
 
-        value = struct.pack('BB', PROP_OFFSET, values[offset])
+        value = struct.pack("BB", PROP_OFFSET, values[offset])
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
     def activate_comfort(self):
         """Activates the comfort temperature."""
-        value = struct.pack('B', PROP_COMFORT)
+        value = struct.pack("B", PROP_COMFORT)
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
     def activate_eco(self):
         """Activates the comfort temperature."""
-        value = struct.pack('B', PROP_ECO)
+        value = struct.pack("B", PROP_ECO)
         self._conn.make_request(PROP_WRITE_HANDLE, value)
 
     @property
@@ -464,4 +483,3 @@ class Thermostat:
     def device_serial(self):
         """Return the device serial number."""
         return self._device_serial
-

eq3bt/eq3cli.py
@@ -1,13 +1,8 @@
 """ Cli tool for testing connectivity with EQ3 smart thermostats. """
-import sys
-if sys.version_info < (3,4):
-    print("To use this script you need python 3.4 or newer! got %s" % sys.version_info)
-    sys.exit(1)
-
 import logging
-import click
 import re
-from click_datetime import Datetime
+
+import click
 
 from eq3bt import Thermostat
 
@@ -15,17 +10,18 @@ pass_dev = click.make_pass_decorator(Thermostat)
 
 
 def validate_mac(ctx, param, mac):
-    if re.match('^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$', mac) is None:
-        raise click.BadParameter(mac + ' is no valid mac address')
+    if re.match("^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$", mac) is None:
+        raise click.BadParameter(mac + " is no valid mac address")
     return mac
 
+
 @click.group(invoke_without_command=True)
-@click.option('--mac', envvar="EQ3_MAC", required=True, callback=validate_mac)
-@click.option('--interface', default=None)
-@click.option('--debug/--normal', default=False)
+@click.option("--mac", envvar="EQ3_MAC", required=True, callback=validate_mac)
+@click.option("--interface", default=None)
+@click.option("--debug/--normal", default=False)
 @click.pass_context
 def cli(ctx, mac, interface, debug):
-    """ Tool to query and modify the state of EQ3 BT smart thermostat. """
+    """Tool to query and modify the state of EQ3 BT smart thermostat."""
     if debug:
         logging.basicConfig(level=logging.DEBUG)
     else:
@@ -40,10 +36,10 @@ def cli(ctx, mac, interface, debug):
 
 
 @cli.command()
-@click.option('--target', type=float, required=False)
+@click.option("--target", type=float, required=False)
 @pass_dev
 def temp(dev, target):
-    """ Gets or sets the target temperature."""
+    """Gets or sets the target temperature."""
     click.echo("Current target temp: %s" % dev.target_temperature)
     if target:
         click.echo("Setting target temp: %s" % target)
@@ -51,10 +47,10 @@ def temp(dev, target):
 
 
 @cli.command()
-@click.option('--target', type=int, required=False)
+@click.option("--target", type=int, required=False)
 @pass_dev
 def mode(dev, target):
-    """ Gets or sets the active mode. """
+    """Gets or sets the active mode."""
     click.echo("Current mode: %s" % dev.mode_readable)
     if target:
         click.echo("Setting mode: %s" % target)
@@ -62,10 +58,10 @@ def mode(dev, target):
 
 
 @cli.command()
-@click.option('--target', type=bool, required=False)
+@click.option("--target", type=bool, required=False)
 @pass_dev
 def boost(dev, target):
-    """ Gets or sets the boost mode. """
+    """Gets or sets the boost mode."""
     click.echo("Boost: %s" % dev.boost)
     if target is not None:
         click.echo("Setting boost: %s" % target)
@@ -75,15 +71,15 @@ def boost(dev, target):
 @cli.command()
 @pass_dev
 def valve_state(dev):
-    """ Gets the state of the valve. """
+    """Gets the state of the valve."""
     click.echo("Valve: %s" % dev.valve_state)
 
 
 @cli.command()
-@click.option('--target', type=bool, required=False)
+@click.option("--target", type=bool, required=False)
 @pass_dev
 def locked(dev, target):
-    """ Gets or sets the lock. """
+    """Gets or sets the lock."""
     click.echo("Locked: %s" % dev.locked)
     if target is not None:
         click.echo("Setting lock: %s" % target)
@@ -93,62 +89,63 @@ def locked(dev, target):
 @cli.command()
 @pass_dev
 def low_battery(dev):
-    """ Gets the low battery status. """
+    """Gets the low battery status."""
     click.echo("Batter low: %s" % dev.low_battery)
 
 
 @cli.command()
-@click.option('--temp', type=float, required=False)
-@click.option('--duration', type=float, required=False)
+@click.option("--temp", type=float, required=False)
+@click.option("--duration", type=float, required=False)
 @pass_dev
 def window_open(dev, temp, duration):
-    """ Gets and sets the window open settings. """
+    """Gets and sets the window open settings."""
     click.echo("Window open: %s" % dev.window_open)
     if dev.window_open_temperature is not None:
         click.echo("Window open temp: %s" % dev.window_open_temperature)
     if dev.window_open_time is not None:
         click.echo("Window open time: %s" % dev.window_open_time)
     if temp and duration:
-        click.echo("Setting window open conf, temp: %s duration: %s" % (temp, duration))
+        click.echo(f"Setting window open conf, temp: {temp} duration: {duration}")
         dev.window_open_config(temp, duration)
 
 
 @cli.command()
-@click.option('--comfort', type=float, required=False)
-@click.option('--eco', type=float, required=False)
+@click.option("--comfort", type=float, required=False)
+@click.option("--eco", type=float, required=False)
 @pass_dev
 def presets(dev, comfort, eco):
-    """ Sets the preset temperatures for auto mode. """
+    """Sets the preset temperatures for auto mode."""
     if dev.comfort_temperature is not None:
         click.echo("Current comfort temp: %s" % dev.comfort_temperature)
     if dev.eco_temperature is not None:
         click.echo("Current eco temp: %s" % dev.eco_temperature)
     if comfort and eco:
-        click.echo("Setting presets: comfort %s, eco %s" % (comfort, eco))
+        click.echo(f"Setting presets: comfort {comfort}, eco {eco}")
         dev.temperature_presets(comfort, eco)
 
 
 @cli.command()
 @pass_dev
 def schedule(dev):
-    """ Gets the schedule from the thermostat. """
+    """Gets the schedule from the thermostat."""
     # TODO: expose setting the schedule somehow?
     for d in range(7):
         dev.query_schedule(d)
     for day in dev.schedule.values():
-        click.echo("Day %s, base temp: %s" % (day.day, day.base_temp))
+        click.echo(f"Day {day.day}, base temp: {day.base_temp}")
         current_hour = day.next_change_at
         for hour in day.hours:
-            if current_hour == 0: continue
-            click.echo("\t[%s-%s] %s" % (current_hour, hour.next_change_at, hour.target_temp))
+            if current_hour == 0:
+                continue
+            click.echo(f"\t[{current_hour}-{hour.next_change_at}] {hour.target_temp}")
             current_hour = hour.next_change_at
 
 
 @cli.command()
-@click.argument('offset', type=float, required=False)
+@click.argument("offset", type=float, required=False)
 @pass_dev
 def offset(dev, offset):
-    """ Sets the temperature offset [-3,5 3,5] """
+    """Sets the temperature offset [-3,5 3,5]"""
     if dev.temperature_offset is not None:
         click.echo("Current temp offset: %s" % dev.temperature_offset)
     if offset is not None:
@@ -157,13 +154,13 @@ def offset(dev, offset):
 
 
 @cli.command()
-@click.argument('away_end', type=Datetime(format='%Y-%m-%d %H:%M'), default=None, required=False)
-@click.argument('temperature', type=float, default=None, required=False)
+@click.argument("away_end", type=click.Datetime(), default=None, required=False)
+@click.argument("temperature", type=float, default=None, required=False)
 @pass_dev
 def away(dev, away_end, temperature):
-    """ Enables or disables the away mode. """
+    """Enables or disables the away mode."""
     if away_end:
-        click.echo("Setting away until %s, temperature: %s" % (away_end, temperature))
+        click.echo(f"Setting away until {away_end}, temperature: {temperature}")
     else:
         click.echo("Disabling away mode")
     dev.set_away(away_end, temperature)
@@ -172,7 +169,7 @@ def away(dev, away_end, temperature):
 @cli.command()
 @pass_dev
 def device(dev):
-    """ Displays basic device information. """
+    """Displays basic device information."""
     dev.query_id()
     click.echo("Firmware version: %s" % dev.firmware_version)
     click.echo("Device serial:    %s" % dev.device_serial)
@@ -181,7 +178,7 @@ def device(dev):
 @cli.command()
 @click.pass_context
 def state(ctx):
-    """ Prints out all available information. """
+    """Prints out all available information."""
     dev = ctx.obj
     click.echo(dev)
     ctx.forward(locked)

pyproject.toml
@@ -1,0 +1,44 @@
+[tool.poetry]
+name = "python-eq3bt"
+version = "0.1.12"
+description = "EQ3 bluetooth thermostat support library"
+license = "MIT"
+authors = ["Markus Peter <mpeter@emdev.de>"]
+maintainers = ["Teemu R. <tpr@iki.fi>"]
+repository = "https://github.com/rytilahti/python-eq3bt"
+readme = "README.md"
+packages = [
+  { include = "eq3bt" }
+]
+include = ["CHANGELOG"]
+
+[tool.poetry.scripts]
+eq3cli = "eq3bt.eq3cli:cli"
+
+[tool.poetry.dependencies]
+python = "^3.7"
+click = "*"
+bluepy = ">=1.0.5"
+construct = "*"
+
+[tool.poetry.dev-dependencies]
+pytest = "*"
+pre-commit = "*"
+toml = "*"
+tox = "*"
+codecov = "*"
+pytest-cov = "*"
+
+
+[tool.isort]
+multi_line_output = 3
+include_trailing_comma = true
+force_grid_wrap = 0
+use_parentheses = true
+line_length = 88
+known_first_party = "eq3bt"
+known_third_party = ["click", "pytest"]
+
+[build-system]
+requires = ["poetry-core>=1.0.0"]
+build-backend = "poetry.core.masonry.api"

.github/workflows/ci.yml
@@ -1,0 +1,54 @@
+name: CI
+
+on:
+  push:
+    branches: ["master"]
+  pull_request:
+    branches: ["master"]
+  workflow_dispatch:  # to allow manual re-runs
+
+
+jobs:
+  linting:
+    name: "Perform linting checks"
+    runs-on: ubuntu-latest
+
+    strategy:
+      matrix:
+        python-version: ["3.10"]
+
+    steps:
+      - uses: "actions/checkout@v2"
+      - uses: "actions/setup-python@v2"
+        with:
+          python-version: "${{ matrix.python-version }}"
+      - name: "Install dependencies"
+        run: |
+          python -m pip install --upgrade pip poetry tox
+          poetry install
+      - name: "Run pre-commit -a"
+        run: |
+          poetry run pre-commit run -a
+
+  tests:
+    name: "Python ${{ matrix.python-version}} on ${{ matrix.os }}"
+    needs: linting
+    runs-on: ${{ matrix.os }}
+
+    strategy:
+      matrix:
+        python-version: ["3.7", "3.8", "3.9", "3.10", "pypy-3.7"]
+        os: [ubuntu-latest]
+
+    steps:
+      - uses: "actions/checkout@v2"
+      - uses: "actions/setup-python@v2"
+        with:
+          python-version: "${{ matrix.python-version }}"
+      - name: "Install dependencies"
+        run: |
+          python -m pip install --upgrade pip poetry
+          poetry install
+      - name: "Run tests"
+        run: |
+          poetry run pytest --cov eq3bt --cov-report xml

.pre-commit-config.yaml
@@ -1,0 +1,37 @@
+repos:
+- repo: https://github.com/pre-commit/pre-commit-hooks
+  rev: v4.1.0
+  hooks:
+  - id: trailing-whitespace
+  - id: end-of-file-fixer
+  - id: check-docstring-first
+  - id: check-yaml
+  - id: debug-statements
+  - id: check-ast
+
+- repo: https://github.com/asottile/pyupgrade
+  rev: v2.31.0
+  hooks:
+  - id: pyupgrade
+    args: ['--py37-plus']
+
+- repo: https://github.com/python/black
+  rev: 22.1.0
+  hooks:
+  - id: black
+
+- repo: https://gitlab.com/pycqa/flake8
+  rev: 3.9.2
+  hooks:
+  - id: flake8
+
+- repo: https://github.com/pre-commit/mirrors-isort
+  rev: v5.10.1
+  hooks:
+  - id: isort
+    additional_dependencies: [toml]
+
+- repo: https://github.com/pre-commit/mirrors-mypy
+  rev: v0.931
+  hooks:
+  - id: mypy

eq3bt/__init__.py
@@ -1,3 +1,3 @@
 # flake8: noqa
-from .eq3btsmart import Thermostat, TemperatureException, Mode
+from .eq3btsmart import Mode, TemperatureException, Thermostat
 from .structures import *

commit 72d831b1c3c65b45093a2fc2c39c2458906d06c9

support gattlib as an alternative btle library (#48)
* fix attribute error click.Datetime

Fixes: 4e19f27c1c74 ("Use poetry, add pre-commit hooks & mass format to modern standards, add CI (#47)")

* support gattlib as an alternative btle library

* Thermostat: default to using bluepy without requiring it

The default should continue being bluepy, but the import of the
connection class can be avoided when a connection class is passed.

* eq3cli: enable using gattlib via a new --backend option

* optionally require installing gattlib

eq3bt/eq3btsmart.py
@@ -15,7 +15,6 @@ from enum import IntEnum
 
 from construct import Byte
 
-from .connection import BTLEConnection
 from .structures import AwayDataAdapter, DeviceId, Schedule, Status
 
 _LOGGER = logging.getLogger(__name__)
@@ -72,7 +71,7 @@ class TemperatureException(Exception):
 class Thermostat:
     """Representation of a EQ3 Bluetooth Smart thermostat."""
 
-    def __init__(self, _mac, _iface=None, connection_cls=BTLEConnection):
+    def __init__(self, _mac, _iface=None, connection_cls=None):
         """Initialize the thermostat."""
 
         self._target_temperature = Mode.Unknown
@@ -95,6 +94,8 @@ class Thermostat:
         self._firmware_version = None
         self._device_serial = None
 
+        if connection_cls is None:
+            from .connection import BTLEConnection as connection_cls
         self._conn = connection_cls(_mac, _iface)
         self._conn.set_callback(PROP_NTFY_HANDLE, self.handle_notification)
 

eq3bt/eq3cli.py
@@ -19,15 +19,21 @@ def validate_mac(ctx, param, mac):
 @click.option("--mac", envvar="EQ3_MAC", required=True, callback=validate_mac)
 @click.option("--interface", default=None)
 @click.option("--debug/--normal", default=False)
+@click.option("--backend", type=click.Choice(["bluepy", "gattlib"]), default="bluepy")
 @click.pass_context
-def cli(ctx, mac, interface, debug):
+def cli(ctx, mac, interface, debug, backend):
     """Tool to query and modify the state of EQ3 BT smart thermostat."""
     if debug:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
 
-    thermostat = Thermostat(mac, interface)
+    if backend == "gattlib":
+        from .gattlibconnection import BTLEConnection
+
+        thermostat = Thermostat(mac, interface, BTLEConnection)
+    else:
+        thermostat = Thermostat(mac, interface)
     thermostat.update()
     ctx.obj = thermostat
 
@@ -154,7 +160,7 @@ def offset(dev, offset):
 
 
 @cli.command()
-@click.argument("away_end", type=click.Datetime(), default=None, required=False)
+@click.argument("away_end", type=click.DateTime(), default=None, required=False)
 @click.argument("temperature", type=float, default=None, required=False)
 @pass_dev
 def away(dev, away_end, temperature):

eq3bt/gattlibconnection.py
@@ -1,0 +1,95 @@
+"""
+A simple adapter to gattlib.
+Handles Connection duties (reconnecting etc.) transparently.
+"""
+import codecs
+import logging
+import threading
+
+import gattlib
+
+DEFAULT_TIMEOUT = 1
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class BTLEConnection:
+    """Representation of a BTLE Connection."""
+
+    def __init__(self, mac, iface):
+        """Initialize the connection."""
+
+        self._conn = None
+        self._mac = mac
+        self._iface = iface
+        self._callbacks = {}
+        self._notifyevent = None
+
+    def __enter__(self):
+        """
+        Context manager __enter__ for connecting the device
+        :rtype: BTLEConnection
+        :return:
+        """
+        _LOGGER.debug("Trying to connect to %s", self._mac)
+        if self._iface is None:
+            self._conn = gattlib.GATTRequester(self._mac, False)
+        else:
+            self._conn = gattlib.GATTRequester(self._mac, False, self._iface)
+        self._conn.on_notification = self.on_notification
+        try:
+            self._conn.connect()
+        except gattlib.BTBaseException as ex:
+            _LOGGER.debug(
+                "Unable to connect to the device %s, retrying: %s", self._mac, ex
+            )
+            try:
+                self._conn.connect()
+            except Exception as ex2:
+                _LOGGER.debug("Second connection try to %s failed: %s", self._mac, ex2)
+                raise
+
+        _LOGGER.debug("Connected to %s", self._mac)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._conn:
+            self._conn.disconnect()
+            self._conn = None
+
+    def on_notification(self, handle, data):
+        """Handle Callback from a Bluetooth (GATT) request."""
+        _LOGGER.debug(
+            "Got notification from %s: %s", handle, codecs.encode(data, "hex")
+        )
+        if handle in self._callbacks:
+            self._callbacks[handle](data[3:])
+        if self._notifyevent:
+            self._notifyevent.set()
+
+    @property
+    def mac(self):
+        """Return the MAC address of the connected device."""
+        return self._mac
+
+    def set_callback(self, handle, function):
+        """Set the callback for a Notification handle. It will be called with the parameter data, which is binary."""
+        self._callbacks[handle] = function
+
+    def make_request(self, handle, value, timeout=DEFAULT_TIMEOUT, with_response=True):
+        """Write a GATT Command without callback - not utf-8."""
+        try:
+            with self:
+                _LOGGER.debug(
+                    "Writing %s to %s",
+                    codecs.encode(value, "hex"),
+                    handle,
+                )
+                self._notifyevent = threading.Event()
+                self._conn.write_by_handle(handle, value)
+                if timeout:
+                    _LOGGER.debug("Waiting for notifications for %s", timeout)
+                    self._notifyevent.wait(timeout)
+        except gattlib.BTBaseException as ex:
+            _LOGGER.debug("Got exception from gattlib while making a request: %s", ex)
+            raise

pyproject.toml
@@ -20,6 +20,10 @@ python = "^3.7"
 click = "*"
 bluepy = ">=1.0.5"
 construct = "*"
+gattlib = { version = "*", optional = true }
+
+[tool.poetry.extras]
+gattlib = ["gattlib"]
 
 [tool.poetry.dev-dependencies]
 pytest = "*"