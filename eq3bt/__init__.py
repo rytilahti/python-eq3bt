90a53eef0f55b596752d015b23cc6dc158187cdc
Teemu R <tpr@iki.fi>
2022-07-10 17:54:00 +0200

    Wrap backend exceptions inside BackendException (#52)

--- eq3bt/__init__.py
+++ eq3bt/__init__.py
@@ -1,3 +1,7 @@
 # flake8: noqa
 from .eq3btsmart import Mode, TemperatureException, Thermostat
 from .structures import *
+
+
+class BackendException(Exception):
+    """Exception to wrap backend exceptions."""
